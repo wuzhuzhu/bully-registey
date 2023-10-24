f515331631080806da2d5f072cd0630f094a83b9
Walter <walterr@live.cn>
2023-10-24 18:02:59 +0800

    [feat]keep keyword in search input

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -52,7 +52,7 @@ const SearchPage = async ({ searchParams, mode = "name" }: PageProps & { mode: '
                     >
                         <AlignJustify className='text-m3sysdarkon-surface-variant my-2 ml-4' />
 
-                        <Input className="" placeholder='输入完整关键字..' type="text" name="keyword" mode='client'
+                        <Input className="" placeholder={searchParams?.keyword || '输入完整关键字..'} type="text" name="keyword" mode='client'
                         />
                         <Button type="submit" variant="ghost" size="icon" className="mr-4">
                             <Search className='text-m3sysdarkon-surface-variant' />
@@ -61,6 +61,9 @@ const SearchPage = async ({ searchParams, mode = "name" }: PageProps & { mode: '
                     </div>
                 </form>
                 <div className="w-full inline-flex flex-col items-center gap-[8px] mt-6">
+                    {pets.length === 0 && searchParams?.keyword && <p className="mx-auto my-8 text-m3sysdarkon-surface">
+                        找不到符合条件的注册,请输入完整关键字
+                    </p>}
                     {pets.length > 0 && pets.map((pet) => (
                         <FilteredPetCard key={`filtered-pet-${pet.id}`} pet={pet} />
                         // <GenderedCardDark
