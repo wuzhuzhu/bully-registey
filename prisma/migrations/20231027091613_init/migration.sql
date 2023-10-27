94b0a8a8bc11c14569a37472ead259d7b3d7d04e
Walter <walterr@live.cn>
2023-10-27 17:18:24 +0800

    [dev]migrate

--- prisma/migrations/20231027091613_init/migration.sql
+++ prisma/migrations/20231027091613_init/migration.sql
@@ -7,6 +7,9 @@ CREATE TYPE "Gender" AS ENUM ('MALE', 'FEMALE');
 -- CreateEnum
 CREATE TYPE "RegistrationStatus" AS ENUM ('PENDING', 'APPROVED', 'REJECTED', 'SUSPENDED');
 
+-- CreateEnum
+CREATE TYPE "FileStatus" AS ENUM ('UPLOADED', 'DELETED');
+
 -- CreateTable
 CREATE TABLE "Account" (
     "id" TEXT NOT NULL,
@@ -38,7 +41,8 @@ CREATE TABLE "Session" (
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
-    "email" TEXT,
+    "name" TEXT DEFAULT 'CBR',
+    "email" TEXT NOT NULL,
     "emailVerified" TIMESTAMP(3),
     "image" TEXT,
 
@@ -50,6 +54,9 @@ CREATE TABLE "Kennel" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "nameEn" TEXT,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Kennel_pkey" PRIMARY KEY ("id")
 );
@@ -60,7 +67,8 @@ CREATE TABLE "Profile" (
     "instagram" TEXT,
     "facebook" TEXT,
     "wechat" TEXT,
-    "mobile" TEXT NOT NULL,
+    "mobile" TEXT,
+    "email" TEXT,
     "userId" TEXT,
     "kennelId" TEXT,
 
@@ -77,21 +85,19 @@ CREATE TABLE "VerificationToken" (
 -- CreateTable
 CREATE TABLE "Pet" (
     "id" TEXT NOT NULL,
-    "path" TEXT NOT NULL,
-    "depth" INTEGER NOT NULL,
-    "numchild" INTEGER NOT NULL DEFAULT 0,
     "name" TEXT NOT NULL,
     "nameEn" TEXT,
-    "ownerName" TEXT NOT NULL,
+    "ownerName" TEXT,
+    "ownerMobile" TEXT,
     "type" "PetType" NOT NULL DEFAULT 'DOG',
     "gender" "Gender" NOT NULL,
     "birthDate" TIMESTAMP(3) NOT NULL,
-    "breed" TEXT NOT NULL,
-    "color" TEXT NOT NULL,
+    "breed" TEXT,
+    "color" TEXT,
+    "location" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "userId" TEXT,
-    "registrationId" TEXT NOT NULL,
+    "createdById" TEXT NOT NULL,
     "kennelId" TEXT,
 
     CONSTRAINT "Pet_pkey" PRIMARY KEY ("id")
@@ -100,14 +106,41 @@ CREATE TABLE "Pet" (
 -- CreateTable
 CREATE TABLE "Registration" (
     "id" TEXT NOT NULL,
+    "readableId" TEXT NOT NULL,
     "status" "RegistrationStatus" NOT NULL DEFAULT 'PENDING',
     "registeredAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "registerEnd" TIMESTAMP(3),
     "reviewedAt" TIMESTAMP(3),
+    "updatedAt" TIMESTAMP(3) NOT NULL,
     "reviewedById" TEXT,
     "attachments" TEXT,
+    "petId" TEXT,
 
     CONSTRAINT "Registration_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "File" (
+    "id" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "size" INTEGER NOT NULL,
+    "kennelId" TEXT,
+    "imgOfId" TEXT,
+    "avatarOfId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "status" "FileStatus" NOT NULL DEFAULT 'UPLOADED',
+    "test" TEXT,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_birth" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
 );
 
 -- CreateIndex
@@ -138,13 +171,28 @@ CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token"
 CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Pet_path_key" ON "Pet"("path");
+CREATE UNIQUE INDEX "Registration_readableId_key" ON "Registration"("readableId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Pet_registrationId_key" ON "Pet"("registrationId");
+CREATE UNIQUE INDEX "Registration_petId_key" ON "Registration"("petId");
 
 -- CreateIndex
-CREATE INDEX "Pet_path_idx" ON "Pet"("path");
+CREATE UNIQUE INDEX "File_key_key" ON "File"("key");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "File_kennelId_key" ON "File"("kennelId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "File_imgOfId_key" ON "File"("imgOfId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "File_avatarOfId_key" ON "File"("avatarOfId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_birth_AB_unique" ON "_birth"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_birth_B_index" ON "_birth"("B");
 
 -- AddForeignKey
 ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -159,13 +207,28 @@ ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId"
 ALTER TABLE "Profile" ADD CONSTRAINT "Profile_kennelId_fkey" FOREIGN KEY ("kennelId") REFERENCES "Kennel"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Pet" ADD CONSTRAINT "Pet_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Pet" ADD CONSTRAINT "Pet_registrationId_fkey" FOREIGN KEY ("registrationId") REFERENCES "Registration"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Pet" ADD CONSTRAINT "Pet_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Pet" ADD CONSTRAINT "Pet_kennelId_fkey" FOREIGN KEY ("kennelId") REFERENCES "Kennel"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Registration" ADD CONSTRAINT "Registration_reviewedById_fkey" FOREIGN KEY ("reviewedById") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Registration" ADD CONSTRAINT "Registration_petId_fkey" FOREIGN KEY ("petId") REFERENCES "Pet"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_kennelId_fkey" FOREIGN KEY ("kennelId") REFERENCES "Kennel"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_imgOfId_fkey" FOREIGN KEY ("imgOfId") REFERENCES "Pet"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_avatarOfId_fkey" FOREIGN KEY ("avatarOfId") REFERENCES "Pet"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_birth" ADD CONSTRAINT "_birth_A_fkey" FOREIGN KEY ("A") REFERENCES "Pet"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_birth" ADD CONSTRAINT "_birth_B_fkey" FOREIGN KEY ("B") REFERENCES "Pet"("id") ON DELETE CASCADE ON UPDATE CASCADE;
